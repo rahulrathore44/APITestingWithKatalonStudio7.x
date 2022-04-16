93e7ceb621417daedfb9321a96e793e3ce5e20a1
rahulrathore44 <rahulrathore44@LRRS>
2022-04-16 09:33:19 -0700

    Added the code to get all variables

--- Scripts/VerifyGlobalVariables/TC - GetAllVariable/Script1650125549064.groovy
+++ Scripts/VerifyGlobalVariables/TC - GetAllVariable/Script1650125549064.groovy
@@ -3,51 +3,27 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-
-import org.assertj.core.api.Assertions
-
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.main.TestCaseMain
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
 import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.testobject.TestObjectProperty
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
 
-import com.kms.katalon.core.testobject.ConditionType
-import com.kms.katalon.core.testobject.RequestObject
+'Use the Class to get all the global variable from a profile'
+def variables = TestCaseMain.getGlobalVariables("default")
 
-'Create the object of Request Object class'
-def requestObject = new RequestObject("RequestObject")
+'Print the map'
+println variables
 
-'Create the header'
-def header = new TestObjectProperty("Accept",ConditionType.EQUALS,"application/json")
-
-'Add the header with request'
-requestObject.setHttpHeaderProperties(Arrays.asList(header))
-
-
-'Configure the Endpoint url'
-requestObject.setRestUrl("http://localhost:8080/laptop-bag/webapi/api/all")
-
-'Configure the type of Request'
-requestObject.setRestRequestMethod("GET")
-
-'Send the request'
-def responseObject = WS.sendRequest(requestObject)
-
-
-'verify the status code'
-Assertions.assertThat(responseObject.getStatusCode()).isEqualTo(200)
-
-//'Print the response body'
-//println responseObject.getResponseText()
-
-'Verify the Response content'
-Assertions.assertThat(responseObject.getResponseText()).asString().contains("Alienware")
+'Print a Specific Variable'
+println variables.get("timeout")
 
---
+++ Test Suites/API Test Suite.ts
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>API Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>62bfa3c0-5c22-4117-a8c9-21832dbcb475</testSuiteGuid>
+   <testCaseLink>
+      <guid>b93fa948-1b6b-45bc-8dfd-a937f3931d05</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DeleteRequest/TC - DeleteRequest</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8be1a2c4-5be5-4031-98a2-44fca3320947</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3c897f1d-5b16-44b8-b19d-c624fcd7c1df</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JwtToken/TC- JwtToken</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>90e65a38-af4b-4031-bada-40bd3785259d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VerifyGetRequest/TC-GetAllEndPoint</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4b9d8d7a-93e5-4251-81d3-34667516acbc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VerifyPostRequest/TC - PostRequest - DynamicBody</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>56fafce8-5db9-49cc-b675-c6b47282e41e</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5a3f0220-eae7-46ff-931b-5b88292c8eff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VerifyPostRequest/TC - PostRequest - DynamicData</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>724e3fec-fc3a-464f-94e4-92df211e509d</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6dc33ea-5950-4cb4-ba06-f594db77e4ae</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VerifyPostRequest/TC-PostRequest-ScriptMode</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8949b634-f245-402b-9474-0ba497d5892f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VerifyPostRequest/TC-PostRequest</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c6b2c9de-1233-4cea-bb0a-371019fdbc93</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c6620fc4-6f3b-4102-beba-c5008a1d5f2a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VerifyPutRequest/TC - PutRequest</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>be11984b-c89e-4121-a4db-df9b541cdb94</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
